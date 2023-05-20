532380c59852de13b367ea733bc33ec7bd66644c
Tristan Paul Ricohermoso <133869194+ezreb0y@users.noreply.github.com>
2023-05-20 08:55:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,42 @@
 import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from PIL import Image
+import numpy as np
 
-# Load CIFAR-10 dataset
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+# Load the CIFAR-10 dataset
+cifar10 = tf.keras.datasets.cifar10
+(_, _), (x_test, _) = cifar10.load_data()
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
 
-# Preprocess the data
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('cifar10_classifier.h5')
+    return model
 
-# Define the CNN model
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dense(10))
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-# Train the model
-model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
-
-# Save the trained model
-model.save('cifar10_model.h5')
-
-# Load the saved model
-model = tf.keras.models.load_model('cifar10_model.h5')
-
-# Define a function to preprocess the image
 def preprocess_image(image):
-    image = image.resize((32, 32))  # Resize the image to (32, 32)
-    image = tf.keras.preprocessing.image.img_to_array(image)  # Convert PIL Image to numpy array
-    processed_image = image / 255.0  # Normalize the image
-    return processed_image
+    image = image.resize((32, 32))  # Resize image to match CIFAR-10 input size
+    image = np.array(image)  # Convert PIL Image to NumPy array
+    image = image / 255.0  # Normalize pixel values between 0 and 1
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    return image
 
-# Define a function to predict the image class
-def predict(image):
-    processed_image = preprocess_image(image)
-    prediction = model.predict(tf.expand_dims(processed_image, axis=0))
-    predicted_class = tf.argmax(prediction, axis=1)[0]
-    return predicted_class
+def import_and_predict(image_data, model):
+    image = preprocess_image(image_data)
+    prediction = model.predict(image)
+    class_index = np.argmax(prediction)
+    class_label = class_names[class_index]
+    return class_label
 
-# Create the Streamlit app
-def main():
-    st.title('CIFAR-10 Image Classification')
-    uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
+model = load_model()
 
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        prediction = predict(image)
-        st.write('Prediction:', prediction)
+st.write("# CIFAR-10 Image Classification")
+file = st.file_uploader("Choose an image file (CIFAR-10 class)", type=["jpg", "png"])
 
-if __name__ == '__main__':
-    main()
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    class_label = import_and_predict(image, model)
+    st.success("Predicted class: " + class_label)
